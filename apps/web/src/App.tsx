10fe1071ab7c5c675cad9d23efa408e6c42373d0
jspinedas <josebpiso16@gmail.com>
2026-02-05 18:33:11 -0500

    feat: agregar enrutamiento y nuevo slice de flujo de pago
    - Instalar react-router-dom
    - Crear paymentFlowSlice con step y paymentIntentStatus
    - Configurar BrowserRouter en main.tsx
    - Agregar rutas: / (ProductPage) y /summary (SummaryPage)
    - Registrar paymentFlowReducer en store

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import ProductPage from './ProductPage';
+import SummaryPage from './SummaryPage';
 
 const App: React.FC = () => {
   return (
-    <ProductPage />
+    <Routes>
+      <Route path="/" element={<ProductPage />} />
+      <Route path="/summary" element={<SummaryPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
 
